d0a01f71c20af1e683c4dc8cba5b3bf883e68287
dieuhoangpham2-gif <dieuhoangpham2@gmail.com>
2026-04-17 00:57:28 +0700

    feat: hoàn thiện giao diện cẩm nang

---
+++ src/data/blogs.js
@@ -0,0 +1,37 @@
+export const blogs = [
+  {
+    id: "1",
+    title: "Cách xịt nước hoa giữ mùi lâu",
+    excerpt: "Giúp hương thơm lưu lại cả ngày",
+    content: `Xịt nước hoa đúng cách giúp mùi hương lưu giữ lâu hơn và tỏa hương tự nhiên suốt cả ngày.
+
+Bạn nên xịt vào các điểm mạch như cổ tay, sau tai, khuỷu tay hoặc sau gáy, vì đây là những vị trí có nhiệt độ ấm giúp khuếch tán hương tốt hơn.
+
+Khi sử dụng, giữ chai cách da khoảng 10–15 cm để mùi hương lan đều, không bị quá nồng.
+
+Tránh chà xát vùng da sau khi xịt vì có thể làm biến đổi mùi và khiến hương bay nhanh hơn.
+
+Ngoài ra, nên xịt lên da sạch và có độ ẩm nhẹ để tăng khả năng giữ mùi.`,
+    image:
+      "https://images.unsplash.com/photo-1759793499819-bf60128a54b4?q=80&w=1470&auto=format&fit=crop",
+  },
+  {
+    id: "2",
+    title: "Phân biệt các nốt hương",
+    excerpt: "Top, Middle, Base note là gì?",
+    content: `Nước hoa được cấu tạo từ 3 tầng hương:
+
+🌿 Hương đầu (Top notes):
+Là mùi hương cảm nhận ngay khi vừa xịt. Bay nhanh sau vài phút.
+
+🌸 Hương giữa (Middle notes):
+Là “trái tim” của nước hoa. Thường lưu 30–60 phút.
+
+🌳 Hương cuối (Base notes):
+Là lớp hương giữ lâu nhất. Có thể lưu nhiều giờ.
+
+👉 Vì vậy, đừng đánh giá nước hoa ngay lúc vừa xịt.`,
+    image:
+      "https://images.unsplash.com/photo-1621240662590-d3eecd0ba64e?q=80&w=774&auto=format&fit=crop",
+  },
+];
